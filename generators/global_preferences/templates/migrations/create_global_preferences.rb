bd1d770756462039152ab968245e9cf5847a3914
Vitaly Kushner <vitaly@astrails.com>
2009-11-12 04:44:49 +0200

    do not create default variables

--- generators/global_preferences/templates/migrations/create_global_preferences.rb
+++ generators/global_preferences/templates/migrations/create_global_preferences.rb
@@ -11,9 +11,6 @@ class CreateGlobalPreferences < ActiveRecord::Migration
       t.timestamps
     end
 
-    GlobalPreference.create!(:name => "application_name", :value => "example")
-    GlobalPreference.create!(:name => "domain", :value => "example.com")
-
     add_index :global_preferences, [:name], :unique => true
   end
 
